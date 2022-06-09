f008868e6bc47e58f910116eb1cc5c983d8ae19d
Zakaria <zaktakk96@gmail.com>
2022-06-09 21:49:11 +0300

    Backend - Added Admin routes

--- laravel-server/routes/api.php
+++ laravel-server/routes/api.php
@@ -14,3 +14,12 @@ Route::group(['middleware' => 'api'], function($router) {
     Route::post('/profile', [JWTController::class, 'profile']);
 });
 
+// Admin APIs
+Route::post('/add_item', [ItemController::class, 'addItem']);
+Route::post('/remove_item', [ItemController::class, 'removeItem']);
+Route::post('/update_item', [ItemController::class, 'updateItem']);
+Route::post('/add_cat', [CategoryController::class, 'addCategory']);
+Route::post('/remove_cat', [CategoryController::class, 'removeCategory']);
+Route::post('/update_cat', [CategoryController::class, 'updateCategory']);
+
+// User APIs
