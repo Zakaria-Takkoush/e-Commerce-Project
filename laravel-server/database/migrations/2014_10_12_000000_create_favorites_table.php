d96a0ad5170b30d219ebfb6243cac447727823ad
Zakaria <zaktakk96@gmail.com>
2022-06-09 19:43:35 +0300

    Backend - Created and migrated tables to database

--- laravel-server/database/migrations/2014_10_12_000000_create_favorites_table.php
+++ laravel-server/database/migrations/2014_10_12_000000_create_favorites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateFavoritesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->bigInteger('user_id');
+            $table->bigInteger('cat_id');
             $table->timestamps();
         });
     }
