d714b6c8a2292e7621db6b940905cd483bf6517f
Zakaria <zaktakk96@gmail.com>
2022-06-10 10:43:19 +0300

    Backend - Created and tested Add Category API

--- laravel-server/app/Http/Controllers/Admin/AdminCategoryController.php
+++ laravel-server/app/Http/Controllers/Admin/AdminCategoryController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class AdminCategoryController extends Controller
 {
-    //
+    // Add Item
+    public function addCategory(Request $request){
+        $category = new Category;
+        $category->name = $request->name;
+        $category->save();
+        
+        return response()->json([
+            "status" => "Success",
+            "category" => $category
+        ], 200);
+    }
 }
