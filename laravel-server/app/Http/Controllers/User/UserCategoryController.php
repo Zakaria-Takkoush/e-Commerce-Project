82c37f530e94ab941c71ec4788994e014cec8337
Zakaria <zaktakk96@gmail.com>
2022-06-10 10:17:38 +0300

    Backend - Organized Controllers

--- laravel-server/app/Http/Controllers/User/UserCategoryController.php
+++ laravel-server/app/Http/Controllers/User/UserCategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class CategoryController extends Controller
+class UserCategoryController extends Controller
 {
     //
 }
--- laravel-server/app/Http/Controllers/User/UserFavoriteController.php
+++ laravel-server/app/Http/Controllers/User/UserFavoriteController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class FavoriteController extends Controller
+class UserFavoriteController extends Controller
 {
     //
 }
