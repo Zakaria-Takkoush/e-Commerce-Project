20221b232b5dd4a72fd0f763c7471c69d8c7163f
Zakaria <zaktakk96@gmail.com>
2022-06-11 21:14:01 +0300

    Frontend - Created Add Item popup window

--- electron-admin/scripts/page.js
+++ electron-admin/scripts/page.js
@@ -1,3 +1,47 @@
+// Pop-up windows
+// Get the modal
+var add_item_popup = document.getElementById("add-item-popup");
+var add_category_popup = document.getElementById("add-category-popup");
+var edit_item_popup = document.getElementById("edit-item-popup");
+var edit_category_popup = document.getElementById("edit-category-popup");
+
+// Get the button that opens the pop-up
+var add_item_btn = document.getElementById("add-item-btn");
+var add_category_btn = document.getElementById("add-category-btn");
+var edit_item_btn = document.getElementById("edit-item-btn");
+var edit_category_btn = document.getElementById("edit-category-btn");
+
+// When the user clicks on the button, open the form
+add_item_btn.onclick = function () {
+    add_item_popup.style.display = "block";
+}
+// add_category_btn.onclick = function () {
+//     add_category_popup.style.display = "block";
+// }
+// edit_item_btn.onclick = function () {
+//     edit_item_popup.style.display = "block";
+// }
+// edit_category_btn.onclick = function () {
+//     edit_category_popup.style.display = "block";
+// }
+
+// When the user clicks anywhere outside of the pop_up, close it
+window.onclick = function (event) {
+  if (event.target == add_item_popup) {
+    add_item_popup.style.display = "none";
+  }
+//   if (event.target == add_category_popup) {
+//     add_category_popup.style.display = "none";
+//   }
+//   if (event.target == edit_item_popup) {
+//     edit_item_popup.style.display = "none";
+//   }
+//   if (event.target == edit_category_popup) {
+//     edit_category_popup.style.display = "none";
+//   }
+}
+
+
 // Get All Categories
 
 let cat_row = ""
@@ -6,10 +50,11 @@ axios({
     url: 'http://127.0.0.1:8000/api/display_cats'
 })
 .then(function (response) {
-    let categories = response.data;
+    let categories = response.data.categories;
     console.log(categories);
 
     for (let i = 0; i < categories.length; i++) {
+        console.log(categories[i]);
         const id = categories[i].id;
         const name = categories[i].name;
     
@@ -23,40 +68,40 @@ axios({
             <td>Delete</td>  
             </tr>`
         }
-        document.getElementById("items").innerHTML += cat_row;
+        document.getElementById("categories").innerHTML += cat_row;
 })
 
-// Get all Items
+// // Get all Items
 
-let item_row = ""
-axios({
-    method: 'get',
-    url: 'http://127.0.0.1:8000/api/display_items'
-})
-    .then(function (response) {
-        let items = response.data;
-        console.log(items);
+// let item_row = ""
+// axios({
+//     method: 'get',
+//     url: 'http://127.0.0.1:8000/api/display_items'
+// })
+//     .then(function (response) {
+//         let items = response.data;
+//         console.log(items);
 
-        for (let i = 0; i < items.length; i++) {
-            const id = items[i].id;
-            const name = items[i].name;
-            const price = items[i].price;
-            const category = items[i].cat_name;
+//         for (let i = 0; i < items.length; i++) {
+//             const id = items[i].id;
+//             const name = items[i].name;
+//             const price = items[i].price;
+//             const category = items[i].cat_name;
         
-            //Apend each new item to the items table
+//             //Apend each new item to the items table
         
-            item_row += `
-                <tr>
-                <td>${id}</td>
-                <td>${name}</td>
-                <td>${price}</td>
-                <td>${category}</td>
-                <td>Edit</td>
-                <td>Delete</td>  
-                </tr>`
-            }
-            document.getElementById("items").innerHTML += item_row;
-})
+//             item_row += `
+//                 <tr>
+//                 <td>${id}</td>
+//                 <td>${name}</td>
+//                 <td>${price}</td>
+//                 <td>${category}</td>
+//                 <td>Edit</td>
+//                 <td>Delete</td>  
+//                 </tr>`
+//             }
+//             document.getElementById("items").innerHTML += item_row;
+// })
 
 
     //     // Adding every item in the database as the below div 
