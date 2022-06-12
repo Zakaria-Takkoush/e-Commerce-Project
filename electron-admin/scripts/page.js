71a903d6a0323e44d941946289fe7f2a578ca1a6
Zakaria <zaktakk96@gmail.com>
2022-06-12 13:49:07 +0300

    Frontend - Implemented "Edit Category"

--- electron-admin/scripts/page.js
+++ electron-admin/scripts/page.js
@@ -72,7 +72,7 @@ axios({
             <tr>
             <td>${id}</td>
             <td>${name}</td>
-            <td><a href="#" id="edit-category-btn"><i class="fa-regular fa-pen-to-square"></i> Edit </a></td>
+            <td><a href="#" onClick="editCategory(${id})" id="edit-category-btn"><i class="fa-regular fa-pen-to-square"></i> Edit </a></td>
             <td><a href="#" onClick="deleteCategory(${id})" id="delete-category-btn"><i class="fa-solid fa-trash"></i> Delete </a></td>  
             </tr>`
             
@@ -267,70 +267,50 @@ function editItem(item_id) {
 
 
 function editCategory(category_id) {
-    let edit_category_name = document.getElementById("edit-category-name").value
-    let data = new FormData();
-    data.append('name', edit_category_name);
-    axios({
-        method: 'post',
-        url: 'http://127.0.0.1:8000/api/update_cat/' + category_id,
-        data: data,
-    })
-    .then(function (response) {
-        console.log(response.data);
-    }
-)
+
+    // show pop-up edit category window
+    edit_category_popup.style.display = "block";
+
+    // get the submit edit button
+    let edit_category = document.getElementById("edit-category")
+
+    // add a click event to the submit edit button
+    edit_category.addEventListener("click", function (event) {
+
+        // define input fields
+        let edit_category_name = document.getElementById("edit-category-name").value
+            event.preventDefault()
+    
+            //Axios Function - Post
+    
+            let data = new FormData();
+            data.append('name', edit_category_name);
+            axios({
+                method: 'post',
+                url: 'http://127.0.0.1:8000/api/update_cat/' + category_id,
+                data: data,
+            })
+            .then(function (response) {
+                // console.log(response.data);
+                location.reload();
+            }
+         )
+        })
 }
 
 
-
-
-    //     // Adding every item in the database as the below div 
-    //         for (let i = 0; i < restaurants.length; i++) {
-    //             table += `
-    //             <tr>
-    //             <td>${restaurants[i].resto_id}</td>
-    //             <td>${restaurants[i].resto_name}</td>
-    //             <td>${restaurants[i].phone_number}</td>
-    //             <td>${restaurants[i].cat_id}</td>
-    //             <td>${restaurants[i].city_id}</td>
-    //             <td>${restaurants[i].description}</td>   
-    //             <td class="edit" onclick="editResto(${restaurants[i].resto_id})" id="edit" ><i class="fa-solid fa-pen-to-square"></i></td>
-    //             <td onclick="deleteResto(${restaurants[i].resto_id})" class="delete" id="delete"><i class="fa-solid fa-xmark"></i></td>
-    //             </tr>`;
-    //         }
-    //         document.getElementById("tbody").innerHTML = table;
-
-
-
-// // This api call is for to get all items in the database and add them into the table of the admin
-// axios({
-//     method: 'get',
-//     url: 'http://127.0.0.1:8000/api/display_items',
-// })
+// function editCategory(category_id) {
+//     let edit_category_name = document.getElementById("edit-category-name").value
+//     let data = new FormData();
+//     data.append('name', edit_category_name);
+//     axios({
+//         method: 'post',
+//         url: 'http://127.0.0.1:8000/api/update_cat/' + category_id,
+//         data: data,
+//     })
 //     .then(function (response) {
-//         let items = response.data;
-//         console.log(items);
-//         // Adding every resto in the database as the below div 
-//             for (let i = 0; i < restaurants.length; i++) {
-//                 table += `
-//                 <tr>
-//                 <td>${restaurants[i].resto_id}</td>
-//                 <td>${restaurants[i].resto_name}</td>
-//                 <td>${restaurants[i].phone_number}</td>
-//                 <td>${restaurants[i].cat_id}</td>
-//                 <td>${restaurants[i].city_id}</td>
-//                 <td>${restaurants[i].description}</td>   
-//                 <td class="edit" onclick="editResto(${restaurants[i].resto_id})" id="edit" ><i class="fa-solid fa-pen-to-square"></i></td>
-//                 <td onclick="deleteResto(${restaurants[i].resto_id})" class="delete" id="delete"><i class="fa-solid fa-xmark"></i></td>
-//                 </tr>`;
-//             }
-//             document.getElementById("tbody").innerHTML = table;
-//             edit.addEventListener("click", function () {
-//                 resto_name.value = "restaurants[idd].resto_name";
-//                 phone_number.value = "restaurants[idd].phone_number";
-//                 city_id.value = "restaurants[idd].city_id";
-//                 cat_id.value = "restaurants[idd].cat_id";
-//                 desc.value = "restaurants[idd].description";
-//             })
-//         }
-//     )
+//         console.log(response.data);
+//     }
+// )
+// }
+
