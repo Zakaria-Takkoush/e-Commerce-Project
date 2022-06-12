d54ba382761494e9b15081c891b46fa14e83af13
Zakaria <zaktakk96@gmail.com>
2022-06-12 17:18:25 +0300

    Frontend - Implemented Login and assigned token to local storage

--- frontend-user/scripts/main.js
+++ frontend-user/scripts/main.js
@@ -46,3 +46,38 @@ register.addEventListener("click", function(event) {
     )
 })
 
+// ----- Login ----- //
+
+
+
+let sign_in = document.getElementById("log-in");
+
+sign_in.addEventListener("click", function(event) {
+    event.preventDefault()
+
+    // Extract credentials
+    let email = document.getElementById("email").value
+    let password = document.getElementById("pass").value
+
+    let data = new FormData();
+    data.append('email', email);
+    data.append('password', password);
+
+    axios({
+    method: 'post',
+    url: 'http://127.0.0.1:8000/api/login',
+    data: data,
+
+    })
+    .then(function (response) {
+        // console.log(response);
+        // console.log(response.data);
+        console.log(response.status)
+        let token = response.data.access_token;
+        if (response.status === 200) {   // if the user exists:
+            user_token = localStorage.setItem("token", token)     // save token in local storage
+            document.location = "landing.html"; // direct the user to the landing page
+        }
+    }
+  )
+})
