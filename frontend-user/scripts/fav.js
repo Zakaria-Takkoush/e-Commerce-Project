28b5ba51c7be589a77c3e5f50f7f9826a2e88a8a
Zakaria <zaktakk96@gmail.com>
2022-06-12 23:04:30 +0300

    Frontend - Accessed User frofile data

---
+++ frontend-user/scripts/fav.js
@@ -0,0 +1,8 @@
+// user_id = 
+
+// // ----- Get All user favs ----- //
+
+// axios.get("http://127.0.0.1:8000/api/get_favs" , { headers: {"Authorization" : `Bearer ${token}`} })
+// .then(res => {
+// console.log(res.data);
+// })
