7e341578bc445469f7b5e4d07304b000f0b1010d
Zakaria <zaktakk96@gmail.com>
2022-06-13 09:36:56 +0300

    Frontend - Implemented "Get Favorites" and display them in the favorites page

--- frontend-user/scripts/fav.js
+++ frontend-user/scripts/fav.js
@@ -4,14 +4,47 @@ document.getElementById('back-nav').onclick = function () {
     document.location = "landing.html"
 }
 
+
+// Get the stored user id given by the token
+
 let user_id = localStorage.getItem("id")
 
+
 // ----- Get All user favs ----- //
+
+let fav_row = ""
 
 axios({
     method: 'get',
     url: 'http://127.0.0.1:8000/api/get_favs/' + user_id
+    // ^^^ Get All Favorites belonging to this user by passing their id
 })
 .then(function (response) {
-    console.log(response)
+    let favs = response.data.favorites
+
+    // Loop through elements and display them
+
+    for (let i = 0; i < favs.length; i++) {
+
+        let name = favs[i].item_name
+        let price = favs[i].item_price
+        // console.log(name + " " + price)
+
+        fav_row += `
+        <div class="item">
+            <div class="item_name">
+                <p>${name}</p>
+            </div>
+            <div class="item_price">
+                <p>${price}</p>
+            </div>
+            <div class="remove">
+                <button>Remove</button>
+            </div>
+        </div>
+        `
+    }
+    document.getElementById("fav-list").innerHTML += fav_row
+
 })
+
